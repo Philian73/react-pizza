0078024aad9166808a487273b92e0dbf2a71d39f
Nikolay Sulteev (Philian73) <Philian73@gmail.com>
2023-10-31 08:23:20 +0300

    format & lint

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import { createRoot } from 'react-dom/client'
 
-import { App } from './app'
+import '@/app/styles/index.scss'
 
-import './app/styles/index.css'
+import { App } from './app'
 
 createRoot(document.getElementById('root')!).render(<App />)
